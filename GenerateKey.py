5d82ca26fb9b7b12b126314673e4546e545726f5
Rupak <69199545+rupak-20@users.noreply.github.com>
2021-06-10 21:59:11 +0530

    Update GenerateKey.py

--- GenerateKey.py
+++ GenerateKey.py
@@ -1,8 +1,6 @@
 from cryptography.fernet import Fernet
 def write_key():
-    """
-    Generates a key and save it into a file
-    """
+    #Generate a key and save it into a file
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
